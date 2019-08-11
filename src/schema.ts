03a3f2ce5ea47f5a0b9526185d4d2afda883b549
chengluyu <chengluyu@live.cn>
2019-08-11 21:46:19 +0800

    docs: add JSDoc for schema.ts

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,10 @@
 import { JSONSchema, RouteSchema, RouteShorthandOptions } from 'fastify';
 import * as tjs from 'typescript-json-schema';
 
+/**
+ * Build a schema generator from current project.
+ * Raise an error if failed to build.
+ */
 function buildSchemaGenerator(): tjs.JsonSchemaGenerator {
   const settings: tjs.PartialArgs = { required: true };
   const program = tjs.programFromConfig('./tsconfig.json');
@@ -14,16 +18,30 @@ function buildSchemaGenerator(): tjs.JsonSchemaGenerator {
 const defaultGenerator = buildSchemaGenerator();
 const allSymbols = new Set(defaultGenerator.getUserSymbols());
 
+/**
+ * Retrieve generated JSON schema from the interface name.
+ * If the symbol does not exists, return `undefined`.
+ * @param symbolName name of the symbol
+ */
 function retriveSchema(symbolName: string): tjs.Definition | undefined {
   return allSymbols.has(symbolName)
     ? defaultGenerator.getSchemaForSymbol(symbolName)
     : undefined;
 }
 
+/**
+ * Shorthand function if you need only schema in route options.
+ * @param name name of the route
+ */
 export function getRouteOptionsFor(name: string): RouteShorthandOptions {
   return { schema: getSchemaFor(name) };
 }
 
+/**
+ * Try to retrieve corresponding interface for query, params, body and
+ * response by name prefix, and assemble them into a schema object.
+ * @param name name of the route
+ */
 export function getSchemaFor(name: string): RouteSchema {
   const response: { [code: number]: JSONSchema, [code: string]: JSONSchema } = {};
   const schemaForOK = retriveSchema(`${name}Response`);
